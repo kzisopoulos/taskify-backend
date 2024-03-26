c87c12d5cbff2cb0e0a69daab6df684e9a0ac6e0
kzisopoulos <k91.zis@gmail.com>
2024-03-26 15:19:55 +0200

    fix: added frontend url to allowed origins

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,5 +1,5 @@
 export const allowedOrigins = [
-  "https://www.thesite.com", // the actuall site when deployed.
+  "https://taskify-kz.vercel.app", // the actuall site when deployed.
   "http://localhost:3000", // e.g the frontend app
   "http://localhost:3001", // e.g the backend app
   "http://localhost:4200",
